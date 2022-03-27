77b6f9118fd3455280a9c86bddd0c3f9ae458fb6
Micah Pixton <micah@pixtons.com>
2022-03-27 14:42:10 -0600

    FEAT: add api for getting and creating fuel ups

---
+++ pages/api/addFuelUp.js
@@ -0,0 +1,13 @@
+const handler = (req, res) => {
+  const method = req.method;
+
+  if (method === "GET") {
+    return res.status(200).json({ msg: "Found" });
+  }
+  if (method === "POST") {
+    return res.status(200).json({ msg: "Created" });
+  }
+  return res.status(404).json({ msg: "Not Authorized" });
+};
+
+export default handler;
