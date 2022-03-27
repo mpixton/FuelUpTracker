2eac00216082545e855fbfba71eda4b4fed0659f
Micah Pixton <micah@pixtons.com>
2022-03-27 14:33:35 -0600

    FEAT: add db connection file

---
+++ utils/db.js
@@ -0,0 +1,10 @@
+const PG_CONFIG = {
+  client: "pg",
+  connection: {
+    user: "fueltracker",
+    password: "secret",
+    host: "localhost",
+    port: 5432,
+    database: "fueltracker",
+  },
+};
