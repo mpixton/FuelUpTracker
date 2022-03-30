648314dbc5dd35ed0708fbb2290defec6610ca4a
Micah Pixton <micah@pixtons.com>
2022-03-29 20:59:58 -0600

    FEAT: add fetch for home page and reorder columns

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -8,6 +8,22 @@ import FuelUpRow from "../src/FuelUpRow";
 export default function Home() {
   const router = useRouter();
   const [fuelUps, setFuelUps] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchFuelUps = () => {
+    fetch("/api/fuelUp")
+      .then((res) => res.json())
+      .then(({ data }) => {
+        setFuelUps(data);
+      })
+      .catch(console.log);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchFuelUps();
+  }, [loading]);
+
   return (
     <>
       <Head>
@@ -24,19 +40,19 @@ export default function Home() {
         <div className={styles.colHeader}>Total Cost</div>
         <div className={styles.colHeader}>Trip</div>
         <div className={styles.colHeader}>Economy</div>
-        {fuelUps.length ? (
+        {fuelUps.length &&
           fuelUps.map((e, i) => (
             <FuelUpRow
               car={e.car}
               gallons={e.gallons}
               key={i.toString()}
               odometer={e.odometer}
-              ppg={e.ppg}
+              ppg={e.price}
               total={e.total}
               trip={e.trip}
             />
-          ))
-        ) : (
+          ))}
+        {!fuelUps.length && (
           <div className={styles.emptyList}>
             No fuel ups yet! Add a fuel up to get started.
           </div>
--- src/FuelUpRow.jsx
+++ src/FuelUpRow.jsx
@@ -5,12 +5,12 @@ const FuelUpRow = ({ total, ppg, gallons, trip, odometer, car, key }) => {
   return (
     <React.Fragment key={key}>
       <div>{car}</div>
-      <div>{trip}</div>
-      <div>{odometer}</div>
-      <div>{gallons}</div>
-      <div>{total}</div>
-      <div>{ppg}</div>
-      <div>{trip / gallons}</div>
+      <div>{odometer} miles</div>
+      <div>{gallons} gallons</div>
+      <div>${ppg}</div>
+      <div>${total}</div>
+      <div>{trip} miles</div>
+      <div>{(trip / gallons).toFixed(2)} mpg</div>
     </React.Fragment>
   );
 };
