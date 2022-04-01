68c1ddd443e6aa018e31c0a5bae56723f6d3a779
Micah Pixton <micah@pixtons.com>
2022-04-01 16:49:10 -0600

    FEAT: add button sizing to component

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import styles from "../styles/Button.module.css";
 
-const Button = ({ type, enabled, text, onClick, color }) => {
+const Button = ({
+  text,
+  onClick,
+  color = "primary",
+  enabled = true,
+  type = "button",
+  size = "normal",
+}) => {
   const buttonStyle = {
     primary: styles.primary,
     secondary: styles.secondary,
     white: styles.white,
     dark: styles.dark,
   };
+
+  const buttonSize = {
+    xs: styles.xs,
+    small: styles.small,
+    normal: "",
+    large: styles.large,
+    xl: styles.xl,
+  };
+
   return (
     <button
       type={type}
       disabled={!enabled}
       onClick={onClick}
-      className={buttonStyle[color]}
+      className={[buttonStyle[color], buttonSize[size], styles.btn].join(" ")}
     >
       {text}
     </button>
