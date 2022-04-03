474fc88c60ea72e5296f7ac064c9efeb72bc67ea
Micah Pixton <micah@pixtons.com>
2022-04-03 14:29:40 -0600

    FEAT: add get route to car

--- pages/api/car.js
+++ pages/api/car.js
@@ -1,10 +1,14 @@
 import knex from "../../knex/knex";
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
   const { method } = req;
 
+  if (method === "GET") {
+    const cars = await knex.select().from("car");
+    return res.status(200).json({ cars: cars });
+  }
   if (method === "POST") {
-    await knex.insert(req.body).into("car")
+    await knex.insert(req.body).into("car");
     return res.status(201).json({ msg: "Created" });
   }
   return res.status(403).json({ msg: "Method Not Allowed" });
